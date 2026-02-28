cadd07835775071e4c70f78098633ba938ae37d2
Ryan Schildknecht <schildknecht618@gmail.com>
2026-02-27 23:27:03 -0600

    Initial Commit

---
+++ patchworkTests/patchworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  patchworkTests.swift
+//  patchworkTests
+//
+//  Created by Ryan Schildknecht on 2/27/26.
+//
+
+import Testing
+@testable import patchwork
+
+struct patchworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
